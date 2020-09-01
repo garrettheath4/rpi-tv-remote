fdb4f6f4406a058bcc4e69aee91e00790b682f6c
Garrett Koller <gkoller@catalist.us>
2020-09-01 12:20:29 -0400

    Updates

---
+++ precache-manifest.eb0ac8aa39dc38f6016e0fb52bc993c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38854d9d3eb837ecdd3b03cd0e2fbc0f",
+    "url": "/rpi-tv-remote/index.html"
+  },
+  {
+    "revision": "cd25b05e2139063eaf53",
+    "url": "/rpi-tv-remote/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "acf65cbe5b367adefee6",
+    "url": "/rpi-tv-remote/static/js/2.f1863037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rpi-tv-remote/static/js/2.f1863037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd25b05e2139063eaf53",
+    "url": "/rpi-tv-remote/static/js/main.d53a7513.chunk.js"
+  },
+  {
+    "revision": "30b272a14d2bf4a02bcf",
+    "url": "/rpi-tv-remote/static/js/runtime-main.a3519f75.js"
+  }
+]);
